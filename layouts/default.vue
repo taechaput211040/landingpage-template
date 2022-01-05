bbd7faf856e7a0ec2405c4940c734b8f5b16a7e5
unknown <thong.fn.realtech@gmail.com>
2022-01-05 18:20:10 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="menu_header">
-      <div class="header-show container d-flex py-3">
+      <div class="header-show container d-flex ">
         <nuxt-link to="/" class="logo">
           <img
             src="https://image.smart-ai-api.com/public/image-storage-betkub/images/20210811100859a9261ec6ff414acaaaa677c9ad7c83a7.png"
@@ -12,19 +12,19 @@
           <nuxt-link
             to="/landingpage"
             :class="{ active_menu: this.$route.name == 'landingpage' }"
-            class="btn mx-1 btn-landing p-2"
+            class="btn mx-1 btn-landing px-4"
             >LANDING PAGE</nuxt-link
           >
           <nuxt-link
             to="/seopage"
             :class="{ active_menu: this.$route.name == 'seopage' }"
-            class="btn mx-1 btn-landing-seo p-2"
+            class="btn mx-1 btn-landing-seo px-4"
             >LANDING PAGE(SEO)</nuxt-link
           >
           <nuxt-link
             to="/memberpage"
             :class="{ active_menu: this.$route.name == 'memberpage' }"
-            class="btn mx-1 btn-member  p-2"
+            class="btn mx-1 btn-member px-4"
             >MEMBER</nuxt-link
           >
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="bg-main-page">
     <div class="bg-header">
-      <div class="bg-none"></div>
       <div class="show-mock">
-        <div class="bg-showmock"></div>
         <img
-          src="https://image.smart-ai-api.com/public/image-storage/demo-template-sale/Rectangle13.png"
+          src="https://image.smart-ai-api.com/public/image-storage/demo-template-sale/Section_Banner_1.png"
           class="img-fluid img-showmock"
           alt=""
         />
       </div>
     </div>
 
-    <div class="show-content container text-center py-md-5 py-2">
-      <div class="col-lg-6 col-md-8 col-12">
-        <h1 class="mt-5 header-content">NEW FEATURE</h1>
-        <h4 class="my-md-5 my-3 description">
+    <div class="show-content container text-center">
+      <div class=" col-md-8 col-12">
+        <h1 class="mt-5 header-content-feature">NEW FEATURE</h1>
+        <h4 class="my-md-4 my-3 description">
           Feather ใหม่ ที่ไม่เหมือนใคร เพื่อเพิ่มความสนุกให้กับ
           เว็บไซต์ของคุณเน้นใช้งานง่ายและตอบโจทย์ที่สุดสำหรับคุณ
         </h4>
@@ -23,25 +21,27 @@
     </div>
     <div class="show-feature container text-center py-md-2 py-2">
       <div class="row">
-        <div class="col-md-3 col-6 p-2">
+        <div class="col-md-3 col-6 p-md-3 p-2">
           <div class="card-feature ">
             <div class="feature"></div>
             <h5>วงล้อ</h5>
           </div>
         </div>
-        <div class="col-md-3 col-6 p-2">
+        <div class="col-md-3 col-6 p-md-3 p-2">
           <div class="card-feature">
-            <div class="feature"></div>
+            <img
+              src="https://image.smart-ai-api.com/public/image-storage/demo-template-sale/spin1.png" class="img-fluid"
+            />
             <h5>เปิดไพ่</h5>
           </div>
         </div>
-        <div class="col-md-3 col-6 p-2">
+        <div class="col-md-3 col-6 p-md-3 p-2">
           <div class="card-feature">
             <div class="feature"></div>
             <h5>เช็คอิน</h5>
           </div>
         </div>
-        <div class="col-md-3 col-6 p-2">
+        <div class="col-md-3 col-6 p-md-3 p-2">
           <div class="card-feature">
             <div class="feature"></div>
             <h5>ลิงก์รับทรัพย์</h5>
