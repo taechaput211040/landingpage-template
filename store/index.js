e15f734e15061e767816fb0b8ab3990b15531bad
unknown <thong.fn.realtech@gmail.com>
2022-01-17 16:23:42 +0700

    update

--- store/index.js
+++ store/index.js
@@ -9,7 +9,9 @@ export default function() {
     actions: {
       async getData(context) {
         return new Promise(async (resolve, reject) => {
-          let response = await this.$axios.get("/get.json");
+          let response = await this.$axios.get(
+            "https://admin-static-api-ehhif4jpyq-as.a.run.app/api/Static/4193b25ca7ceee6b8d0a7e4d38194f83"
+          );
           if (response.status == 200) {
             localStorage.setItem(
               "itemrender",
